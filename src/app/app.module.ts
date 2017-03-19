0de100da50f3639143459201f97b1ee5a794650e
jesse <jsoldat@hotmail.com>
2017-03-19 11:43:44 +0700

    generated components added routes and setup navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
+import { EditListingComponent } from './edit-listing/edit-listing.component';
+import { ListingComponent } from './listing/listing.component';
+import { ListingsComponent } from './listings/listings.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyCZ0Kt_NXjhwekjY7a_7KXPcMvu5cAjPWI",
+  authDomain: "angular2-c77fe.firebaseapp.com",
+  databaseURL: "https://angular2-c77fe.firebaseio.com",
+  storageBucket: "angular2-c77fe.appspot.com",
+  messagingSenderId: "155601858627"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+};
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listings', component: ListingsComponent},
+  {path: 'listing:/id', component: ListingComponent},
+  {path: 'add-listing', component: AddListingComponent},
+  {path: 'edit-listing/:id', component: EditListingComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddListingComponent,
+    EditListingComponent,
+    ListingComponent,
+    ListingsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlashMessagesModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
